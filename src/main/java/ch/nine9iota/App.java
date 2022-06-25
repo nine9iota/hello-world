5a17f90be1ff0156c353b25ad5cce9cbc8293ce3
nine9iota <leon20.zh@gmail.com>
2022-06-25 15:49:49 +0200

    Initial commit

---
+++ src/main/java/ch/nine9iota/App.java
@@ -0,0 +1,15 @@
+package ch.nine9iota;
+
+/**
+ * Hello world!
+ * @author nine9iota
+ * @version 1.0.0
+ */
+
+public class App {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Hello World");
+        }
+    }
+}
